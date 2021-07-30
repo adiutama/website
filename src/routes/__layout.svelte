3b8dd1f4d3df0cd40c974077eb1e4f4ff6adf104
Adi Utama <neogatotkaca@gmail.com>
2021-07-31 03:07:00 +0700

    feat: home page

---
+++ src/routes/__layout.svelte
@@ -0,0 +1,9 @@
+<div class="h-screen overflow-auto bg-[#222831] text-[#DDDDDD]">
+  <slot></slot>
+</div>
+
+<style windi:preflights:global windi:safelist:global>
+  :global(body) {
+    font-family: 'Poppins', sans-serif;
+  }
+</style>
--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,2 +1,18 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script>
+	import { GithubIcon, InstagramIcon, LinkedinIcon } from 'svelte-feather-icons';
+</script>
+
+<svelte:head>
+	<title>Adi Utama</title>
+</svelte:head>
+
+<div class=" max-w-sm p-14">
+	<h1 class="text-lg">Hi, I’m <span class="text-[#B5CDA3]">Adi Utama</span> 👋</h1>
+	<p class="mt-6">I’m a Software Developer from Yogyakarta, Indonesia.</p>
+
+	<div class="flex mt-14">
+		<a href="https://github.com/adiutama/" class="mx-2 hover:text-[#B5CDA3]"><GithubIcon size="1.5x" /></a>
+		<a href="https://instagram.com/adiutama/" class="mx-2 hover:text-[#B5CDA3]"><InstagramIcon size="1.5x" /></a>
+		<a href="https://www.linkedin.com/in/adiutamaa/" class="mx-2 hover:text-[#B5CDA3]"><LinkedinIcon size="1.5x" /></a>
+	</div>
+</div>
